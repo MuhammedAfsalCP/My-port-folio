eef9e9bb886a38f42de095f09bc487fe83a30050
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2024-11-20 18:45:54 +0530

    new

--- style.js
+++ style.js
@@ -1,15 +1,15 @@
 function sentMail() {
+
     if (document.getElementById("email_name").value.length > 0 &&
         document.getElementById("email_address").value.length > 0 &&
-        document.getElementById("email_message").value.length > 0 &&
-        document.getElementById("input_number").value.length ==10
+        document.getElementById("email_message").value.length > 0
     ) {
         var params = {
             name: document.getElementById("email_name").value,
             email: document.getElementById("email_address").value,
             message: document.getElementById("email_message").value,
-            number: document.getElementById("input_number").value,
-            address: document.getElementById("input_details").value,
+            
+           
         };
         const serviceID = "service_btlwrap";
         const templateID = "template_r10g6c1";
@@ -19,8 +19,7 @@ function sentMail() {
                 document.getElementById("email_name").value = "";
                 document.getElementById("email_address").value = "";
                 document.getElementById("email_message").value = "";
-                document.getElementById("input_number").value = "";
-                document.getElementById("input_details").value = "";
+                
                 console.log(res);
                 alert("Your Message Sent Successfully");
             }
@@ -35,11 +34,5 @@ function sentMail() {
     }
     else if (document.getElementById("email_message").value.length == 0) {
         alert("Message cant be empty");
-    } else if (document.getElementById("input_number").value.length == 0) {
-        alert("Number cant be empty");
-    } else if (document.getElementById("input_number").value.length < 10) {
-        alert("Number cant be Full");
-    } else if (document.getElementById("input_number").value.length > 10) {
-        alert("Enter 10 Digit Number");
     }
 }
